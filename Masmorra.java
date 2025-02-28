5b08e1bea57d4c97b4f9c9b59bf9f16d0ab5047e
Someone8643 <oriol.casas@insbaixcamp.cat>
2025-02-28 18:44:37 +0100

    Tests + arreglar

--- Masmorra.java
+++ Masmorra.java
@@ -1,23 +1,43 @@
 import java.util.Scanner;
 
 public class Masmorra {
+
+    // Finalment, hi haurà una classe masmorra (estàtica) per controlar el joc:
+    // Contenir els arrays de tots els tresors i monstres que hi haurà al joc.
+    // Crear la masmorra (MxN) i omplir-la de sales aleatòriament.
+    // Impressió de la masmorra amb les sales explorades (*), sales sense explorar (-) i posició actual del personatge (&).
+    // Mostrar opcions (moure, explorar i/o atacar).
+    // Comprovar si el joc ha finalitzat o no i mostrar resultats.
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvengut joc DungeonQuest");
         System.out.println("estas prepart per jugar (si o no)");
         String name = sc.nextLine();
-        if (name.equals("si")) {
-            System.out.println("Joc DungeonQuest");
-            System.out.println("....................");
+        if (name.equalsIgnoreCase("si")) {
+            System.out.println("-------------------");
+            System.out.println("----DungeonQuest---");
+            System.out.println("-------------------");
         }else {
-            System.out.println("Cuant estes preparat entra a jugar ");
+            System.out.println("Quan estiguis preparat, entra a jugar!");
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-    //Finalment, hi haurà una classe masmorra (estàtica) per controlar el joc:
-    //Contenir els arrays de tots els tresors i monstres que hi haurà al joc.
-    //Crear la masmorra (MxN) i omplir-la de sales aleatòriament.
-    //Impressió de la masmorra amb les sales explorades (*), sales sense explorar (-) i posició actual del personatge (&).
-    //Mostrar opcions (moure, explorar i/o atacar).
-    //Comprovar si el joc ha finalitzat o no i mostrar resultats.
+
 }
