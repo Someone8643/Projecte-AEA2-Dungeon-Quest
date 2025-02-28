9faebc746d87195e7210fad271b04e003e08d206
Someone8643 <oriol.casas@insbaixcamp.cat>
2025-02-28 17:47:25 +0100

    Continuar classe Numero aleatori

--- NumeroAleatori.java
+++ NumeroAleatori.java
@@ -2,13 +2,15 @@ import java.util.Random;
 
 public class NumeroAleatori {
 
-    // Propietats
+    //// Propietats
+    private static Random aleatori = new Random();
 
+    //// Constructors
+    public NumeroAleatori() {
+        // default
+    }
 
-    // Constructors
-
-    // Mètodes
-
+    //// Mètodes
     /**
      * Mètode per generar un enter dintre d'un rang.
      * @param valorMinim El mínim valor que es pot generar.
@@ -20,9 +22,9 @@ public class NumeroAleatori {
         // Utilitzem la classe random (mirar la seva documentació)
         // Hem d'utilitzar un dels seus mètodes per obtenir un int aleatori del 1 al màxim (mínim 1 per la suma i el màxim perquè aquest mètode agafa el màxim de forma no inclosa).
 
-        Random aleatori = new Random();
         return aleatori.nextInt(valorMaxim - valorMinim + 1) + valorMinim;
     }
+
 
     /**
      * Mètode per generar un enter dintre d'un rang.
@@ -33,11 +35,44 @@ public class NumeroAleatori {
     public static double generarDoubleAleatoriRang(double valorMinim, double valorMaxim) {
 
         // Utilitzem la classe random (mirar la seva documentació)
-        // Hem d'utilitzar un dels seus mètodes per obtenir un int aleatori del 1 al màxim (mínim 1 per la suma i el màxim perquè aquest mètode agafa el màxim de forma no inclosa).
+        // Hem d'utilitzar un dels seus mètodes per obtenir un double aleatori del 0,1 al màxim (mínim 0,1 per la suma i el màxim perquè aquest mètode agafa el màxim de forma no inclosa).
 
-        Random aleatori = new Random();
         return aleatori.nextDouble(valorMaxim - valorMinim + 0.1) + valorMinim;
     }
 
+
+    /**
+     * Mètode que retorna cert o fals de forma aleatòria.
+     * @return Cert o fals de forma aleatòria.
+     */
+    public static boolean tirarMoneda() {
+
+        // Cridem a generar int amb mínim 0 i màxim 1, si és 0, false, si és 1, true.
+
+        if (NumeroAleatori.generarIntAleatoriRang(0, 1) == 0) {
+
+            return false;
+        } else {
+
+            return true;
+        }
+    }
+
+    /**
+     * Mètode per obtenir cert o fals segons una probabilitat en percentatge. Un percentatge alt retornarà cert més vegades mentre que un de més baix menys vegades.
+     * @param percentatge Un enter que representa el percentatge de vegades que es retornaria cert.
+     * @return Cert o fals de forma aleatòria.
+     */
+    public static boolean percentatgeProbabilitat(int percentatge) {
+
+        // Utilitzem el random int per fer una probabilitat de retornar cert segons el percentatge (enter) que ens passen.
+
+        if (NumeroAleatori.generarIntAleatoriRang(0, 100) <= percentatge) {
+
+            return true;
+        }
+
+        return false;
+    }
 
 }
