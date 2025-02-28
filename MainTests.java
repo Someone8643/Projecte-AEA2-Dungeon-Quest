29c29b51ebc91ce65b462049429ed93eae984492
Someone8643 <oriol.casas@insbaixcamp.cat>
2025-02-28 18:29:26 +0100

    Tests + arreglar

--- MainTests.java
+++ MainTests.java
@@ -7,6 +7,26 @@ public class MainTests {
         System.out.println(Aleatori.generarDoubleAleatoriRang(0, 5));
 
 
+        System.out.println(Aleatori.tirarMoneda());
+
+        int vegadescert = 0;
+        for (int i = 0; i < 100; i++) {
+
+            if (Aleatori.percentatgeProbabilitat(70)) {
+                vegadescert++;
+            }
+
+        }
+
+        System.out.println(vegadescert);
+
+
+
+
+
+        Aleatori.generarIntAleatoriRang(1,5);
+
+
     }
 
 
--- Persontge.java
+++ Persontge.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Persontge {
-    //propiedades
+    // Propiedades
     private String nom;
     private int vida;
     private int atac;
